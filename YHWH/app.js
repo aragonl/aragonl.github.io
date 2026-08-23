b652c81987c7346cc19ba60183cce6baf52c45e9
Luis Aragon <aragon_l@hotmail.com>
2026-08-23 10:14:31 +0200

    Actualizar app.js

--- YHWH/app.js
+++ YHWH/app.js
@@ -1,5 +1,5 @@
 import { WORD_DATA } from "./palabras-it.js";
-import { TEXT, t, EMOJI_MAP } from "./texto-it.js";
+import { TEXT, t, EMOJI_MAP } from "./text-it.js";
 
 const $ = (id) => document.getElementById(id);
 
