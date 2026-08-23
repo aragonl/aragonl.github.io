860aa84afe2754977b0c2f5aa642133fefe5e0c4
Luis Aragon <aragon_l@hotmail.com>
2026-08-23 07:28:00 +0200

    De esp a italiano

--- YHWH/app.js
+++ YHWH/app.js
@@ -1,5 +1,5 @@
-import { WORD_DATA } from "./palabras-es.js";
-import { TEXT, t, EMOJI_MAP } from "./texto-es.js";
+import { WORD_DATA } from "./palabras-it.js";
+import { TEXT, t, EMOJI_MAP } from "./texto-it.js";
 
 const $ = (id) => document.getElementById(id);
 
